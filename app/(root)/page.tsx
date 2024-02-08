7b94c2a4dc7a3eb8ce01317c29074caad4c006b8
Sirasit Punnanithi <spunnanithi@gmail.com>
2024-02-07 22:30:59 -0800

    Set up 'root' app group and installed button from shadcn/ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return (
+		<div className="p-10">
+			<p>Hello Admin Dashboard</p>
+			<Button variant="destructive">Button</Button>
+		</div>
+	);
+}

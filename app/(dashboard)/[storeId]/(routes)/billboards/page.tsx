f28ae1b1122630687af18fe3a117bf3a8cd3e33a
Sirasit Punnanithi <spunnanithi@gmail.com>
2024-02-11 10:03:16 -0800

    Added pagination to data table for billboards

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -14,11 +14,11 @@ const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
     },
   });
 
-  const formattedBillboards: BillboardColumn[] = billboards.map((item) => {
-    id: item.id;
-    label: item.label;
-    createdAt: format(item.createdAt, "MMMM do, yyyy");
-  });
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
